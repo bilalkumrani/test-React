0448df204538de8607ba16cf85be0d6cd7ae6de0
Bilal Umrani <bilalumrani@proton.me>
2025-04-10 22:22:47 +0500

    Commit from TeleportHQ

---
+++ src/views/variant4.js
@@ -0,0 +1,248 @@
+import React from 'react'
+
+import { Helmet } from 'react-helmet'
+
+import './variant4.css'
+
+const Variant4 = (props) => {
+  return (
+    <div className="variant4-container">
+      <Helmet>
+        <title>exported project</title>
+      </Helmet>
+      <div className="variant4-variant4">
+        <img
+          alt="Rectangle6423"
+          src="/external/rectangle6423-z4u-1500w.png"
+          className="variant4-rectangle64"
+        />
+        <img
+          alt="Rectangle6324"
+          src="/external/rectangle6324-rhn-900w.png"
+          className="variant4-rectangle63"
+        />
+        <div className="variant4-group66">
+          <img
+            alt="Subtract27"
+            src="/external/subtract27-7m5.svg"
+            className="variant4-subtract"
+          />
+          <div className="variant4-group6">
+            <span className="variant4-text10">Order Approved</span>
+            <img
+              alt="Vector212"
+              src="/external/vector212-up3m.svg"
+              className="variant4-vector1"
+            />
+          </div>
+          <div className="variant4-group8">
+            <span className="variant4-text11">Customize Order</span>
+            <div className="variant4-group7">
+              <img
+                alt="Vector216"
+                src="/external/vector216-echer.svg"
+                className="variant4-vector2"
+              />
+            </div>
+          </div>
+          <div className="variant4-group10">
+            <span className="variant4-text12">Final Confirmation</span>
+            <div className="variant4-group9">
+              <img
+                alt="Vector221"
+                src="/external/vector221-8jejn.svg"
+                className="variant4-vector3"
+              />
+            </div>
+          </div>
+        </div>
+        <img
+          alt="ScreenShot20220429at20551223"
+          src="/external/screenshot20220429at20551223-4yke-500h.png"
+          className="variant4-screen-shot20220429at20551"
+        />
+        <span className="variant4-text13">
+          <span className="variant4-text14">
+            Address:
+            <span
+              dangerouslySetInnerHTML={{
+                __html: ' ',
+              }}
+            />
+          </span>
+          <span>499 Evernia Street, Apt 303 West Palm Beach, FL 33401</span>
+          <br></br>
+          <br></br>
+        </span>
+        <span className="variant4-text18">
+          Content, images and assets on this site may not be reproduced,
+          transmitted, cached, distributed, or otherwise used, without the prior
+          written consent of Svelte Media, Inc. This site does not provide any
+          professional medical advice or services. The ideas, procedures, and
+          suggestions contained here are not a substitute for consulting with
+          your physician, and should only be used as part of planning your
+          development of an overall health and fitness plan with your physician.
+          All matters regarding your health require medical supervision. Our
+          Terms of Service limit our liability to you, and you are required to
+          read and agree to them before using our site. This information is not
+          intended to diagnose, treat, cure, or prevent any disease. Your
+          results and experience may be different from any claims or
+          testimonials on this site. The FTC requires that we identify what a
+          &quot;typical&quot; result is. Typically people never do anything with
+          products and information that they buy, so in most cases they
+          won&apos;t get any results. The statements and photos you see on our
+          site are examples of individuals that took action in changing their
+          lifestyle, and the results are not typical. If you&apos;re looking for
+          results, you need to take action and stick to the plan!
+        </span>
+        <div className="variant4-group65">
+          <span className="variant4-text19">
+            © 2022 Svelte Media, Inc., All Rights Reserved
+            <span
+              dangerouslySetInnerHTML={{
+                __html: ' ',
+              }}
+            />
+          </span>
+          <span className="variant4-text20">
+            Privacy Policy | Terms of Services | Contact Us
+            <span
+              dangerouslySetInnerHTML={{
+                __html: ' ',
+              }}
+            />
+          </span>
+          <span className="variant4-text21">
+            By clicking accepting the order above, your payment method will be
+            charged the total price above + sales tax for your region if
+            applicable. Your purchase is 100% Secure and backed by our 60 Day
+            Money Back Guarantee.
+          </span>
+        </div>
+        <img
+          alt="ScreenShot20220422at13171230"
+          src="/external/screenshot20220422at13171230-kd1-200h.png"
+          className="variant4-screen-shot20220422at13171"
+        />
+        <img
+          alt="ScreenShot20220422at13172231"
+          src="/external/screenshot20220422at13172231-i2xuc-200h.png"
+          className="variant4-screen-shot20220422at13172"
+        />
+        <img
+          alt="Rectangle65232"
+          src="/external/rectangle65232-63d-300h.png"
+          className="variant4-rectangle65"
+        />
+        <img
+          alt="METABUNDLEMockup1233"
+          src="/external/metabundlemockup1233-mwkg-300h.png"
+          className="variant4metabundle-mockup1"
+        />
+        <img
+          alt="MetaCleanMockup234"
+          src="/external/metacleanmockup234-dxrh-200w.png"
+          className="variant4-meta-clean-mockup"
+        />
+        <img
+          alt="MetaLeanMockup235"
+          src="/external/metaleanmockup235-5saq-200w.png"
+          className="variant4-meta-lean-mockup"
+        />
+        <span className="variant4-text22">
+          <span>Add Your Ultimate MetaPackage To Your Order Right Now!</span>
+          <br></br>
+        </span>
+        <span className="variant4-text25">
+          <span className="variant4-text26">The MetaBreakfast Cookbook:</span>
+          <span className="variant4-text27">
+            <span
+              dangerouslySetInnerHTML={{
+                __html: ' ',
+              }}
+            />
+          </span>
+          <span className="variant4-text28">
+            Quick, healthy recipes that boost your metabolism for all day fat
+            burning!
+          </span>
+          <br></br>
+          <span className="variant4-text30">The MetaLunches Cookbook:</span>
+          <span className="variant4-text31">
+            <span
+              dangerouslySetInnerHTML={{
+                __html: ' ',
+              }}
+            />
+          </span>
+          <span className="variant4-text32">
+            Simple and savory meals packed with MetaInfluencer superfoods!
+          </span>
+          <br></br>
+          <span className="variant4-text34">The MetaDinners Cookbook:</span>
+          <span>
+            {' '}
+            Delicious metabolism boosting dinner classics the whole family will
+            love! 
+          </span>
+        </span>
+        <span className="variant4-text36">
+          <span>
+            Get your 3 fat melting cookbooks &amp; 2 bonus reports for a special
+            discounted price on this page ONLY!
+          </span>
+          <br></br>
+          <br></br>
+          <span>
+            Don’t risk missing out on the simple, body slimming recipes in this
+            done-for-you system! 
+          </span>
+          <br></br>
+        </span>
+        <span className="variant4-text42">
+          <span
+            dangerouslySetInnerHTML={{
+              __html: ' ',
+            }}
+          />
+        </span>
+        <span className="variant4-text43">
+          <span className="variant4-text44">
+            MetaClean Ultimate Fat Burn Book
+          </span>
+          <span className="variant4-text45">
+            {' '}
+            - Nutritious power-shots and elixirs that flush out fat, toxins and
+            impurities.
+          </span>
+          <br></br>
+          <span className="variant4-text47">
+            MetaLean Ultimate Slim Down Book
+          </span>
+          <span> - Delicious and easy-to-make body slimming smoothies!</span>
+        </span>
+        <span className="variant4-text49">PLUS...</span>
+        <span className="variant4-text50">
+          NO THANKS, I do not want to dramatically enhance and accelerate my
+          metabolism with a variety of great tasting, fat-burning recipes.
+        </span>
+        <div className="variant4idea7">
+          <div className="variant4-frame11">
+            <span className="variant4-text51">
+              Yes! Supercharge My Fat Loss – Add Now!
+            </span>
+          </div>
+        </div>
+        <div className="variant4idea8">
+          <div className="variant4-frame12">
+            <span className="variant4-text52">
+              Yes! Supercharge My Fat Loss – Add Now!
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Variant4
